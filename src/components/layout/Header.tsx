3ee39d6d15ae08758003ba977bc9c3b969e0c1ea
Mbang Ernest N <mbangernest@yahoo.com>
2026-02-10 13:53:38 +0100

    fix header and footer

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -159,7 +159,17 @@ export default function Header() {
                   <svg className="w-4 h-4 text-emerald-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                   </svg>
-                  <span className="font-medium">hovulfcameroon@gmail.com</span>
+                  <span className="font-medium">
+                    <a
+                      href="mailto:hovulfcameroon@gmail.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:text-beige-400 transition-colors"
+                    >
+                      hovulfcameroon@gmail.com
+                    </a>
+                  </span>
+
                 </div>
               </div>
               <div className="flex items-center gap-4">
@@ -258,7 +268,17 @@ export default function Header() {
                     <svg className="w-4 h-4 text-emerald-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                     </svg>
-                    <span className="font-medium">hovulfcameroon@gmail.com</span>
+                    <span className="font-medium">
+                          <a
+                            href="mailto:hovulfcameroon@gmail.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-beige-400 transition-colors"
+                          >
+                            hovulfcameroon@gmail.com
+                          </a>
+                        </span>
+
                   </div>
                 </div>
               </div>
